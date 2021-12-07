94a9a0c619e630e9ec7d0ec639d4b49ff533c8a8
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2021-12-06 18:15:28 -0800

    user registration implemented in with anglur UI

--- AngularClient/src/app/register/register.component.ts
+++ AngularClient/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -7,16 +8,19 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
  model: any = {};
- @Input()  usersFromHomeComponent: any;
  @Output() cancelRegister = new EventEmitter();
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   register(){
-    console.log(this.model);
+   this.accountService.register(this.model).subscribe(response => {
+     this.cancel();
+   }, error => {
+     console.log(error);
+   })
   }
 
   cancel(){
